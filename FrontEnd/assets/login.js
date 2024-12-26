f248b0b22813fa687fc6482a9692fbbce568ef87
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2024-12-26 13:17:11 +0100

    Création d'erreur sur un mauvais mail ou mot de passe

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,30 +1,67 @@
+function genererErrorBox () {
+    let errorbox = document.getElementById("errorBox")
+
+            if (!errorbox) {
+                errorbox = document.createElement("div")
+                errorbox.id = "errorBox"
+                errorbox.innerText = "E-mail ou mot de passe incorrect veuillez ressayer."
+                document.getElementById("loginFrom").prepend(errorbox)
+            }
+}
+
+
 async function loginUser () {
 
     const form = document.getElementById("loginFrom")
-
+    
     form.addEventListener("submit", async function(event) {
         event.preventDefault()
+        
+        let username = document.getElementById("email");
+        let password = document.getElementById("motdepasse");
 
-        const username = document.getElementById("email").value;
-        const password = document.getElementById("motdepasse").value;
         const donneelogin = {
-            "email": `${username}`,
-            "password": `${password}`
-          }
+            "email": `${username.value}`,
+            "password": `${password.value}`
+        }
+
         const chargeUtile = JSON.stringify(donneelogin)
 
-        const reponse = await fetch ("http://localhost:5678/api/users/login",{
-            method : "POST",
-            headers : {"Content-Type": "application/json"},
-            body : chargeUtile
-        })
+        username.value = ""
+        password.value = ""
 
-        const user = await reponse.json()
+        try {
+            
+            const reponse = await fetch ("http://localhost:5678/api/users/login",{
+                method : "POST",
+                headers : {"Content-Type": "application/json"},
+                body : chargeUtile
+            })
 
-        console.log(user.token)
+            if (reponse.status != 200){
+
+                throw new Error()
+
+            } else {
+
+                const user = await reponse.json()
+                const token = user.token
+
+                sessionStorage.setItem("authToken", token)
+            }
+
+        } catch {
+            
+            genererErrorBox ()
+            
+        }
 
     })
-
+   
 }
 
 loginUser ()
+
+
+
+
